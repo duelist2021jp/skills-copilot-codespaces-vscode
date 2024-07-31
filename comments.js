253a24d9b229ee27e973758ec406cd128fc93128
duelist2021jp <79241302+duelist2021jp@users.noreply.github.com>
2024-07-31 16:06:04 +0900

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,65 +1,17 @@
 // Create Web Server
-var express = require('express');
-var app = express();
+// Create a web server that listens on port 3000 
+// Use the fs module to read the file comments.json
+// Respond with the contents of the file
+
 var fs = require('fs');
-var path = require('path');
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var http = require('http');
 
-// Create a server
-var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Server is listening at http://%s:%s", host, port);
-});
-
-// Read data from file
-var data = fs.readFileSync("comments.json");
-var comments = JSON.parse(data);
-
-// Get all comments
-app.get('/comments', function (req, res) {
-    res.end(JSON.stringify(comments));
-});
-
-// Post a comment
-app.post('/comment', function (req, res) {
-    var comment = req.body;
-    comments.push(comment);
-    fs.writeFile("comments.json", JSON.stringify(comments), function (err) {
-        if (err) {
-            console.log(err);
-        }
+var server = http.createServer(function(req, res) {
+    fs.readFile('./comments.json', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(data);
     });
-    res.end(JSON.stringify(comments));
 });
 
-// Delete a comment
-app.delete('/comment/:id', function (req, res) {
-    var id = req.params.id;
-    if (id < comments.length) {
-        comments.splice(id, 1);
-    }
-    fs.writeFile("comments.json", JSON.stringify(comments), function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    res.end(JSON.stringify(comments));
-});
-
-// Update a comment
-app.put('/comment/:id', function (req, res) {
-    var id = req.params.id;
-    var comment = req.body;
-    if (id < comments.length) {
-        comments[id] = comment;
-    }
-    fs.writeFile("comments.json", JSON.stringify(comments), function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    res.end(JSON.stringify(comments));
-});
+server.listen(3000);
+// Create a web server that listens on port 3000 
